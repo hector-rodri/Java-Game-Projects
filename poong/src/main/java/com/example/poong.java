b81c213f6e52eeb728f5dded129e56448a957266
hector-rodri <hrodriguez@nigul.cide.es>
2025-03-29 15:12:25 +0100

    importación de librerias y jframe

---
+++ poong/src/main/java/com/example/poong.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import javax.swing.*;//Importamos las librerias necesarias
+import java.awt.*;
+import java.awt.event.*;
+
+public class poong {
+    public static void main(String[] args) {
+        JFrame ventana = new JFrame("Poong");
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//
+        ventana.setSize(800, 600);
+        ventana.setLocationRelativeTo(null);
+        ventana.setResizable(false);
+        ventana.setVisible(true);
+
+    }
+
+
+
+
+
+
+}
