0d336d080f0fd89b185f97d8909c989717def884
hector-rodri <hrodriguez@nigul.cide.es>
2025-04-21 18:39:23 +0200

    pantalla principal

--- streetFighterLite/src/main/java/com/example/Main.java
+++ streetFighterLite/src/main/java/com/example/Main.java
@@ -8,11 +8,13 @@ import java.awt.event.*;
 public class Main extends JFrame {
 
     private JPanel panelPrincipal;
-    Timer timer;
-    int counter = 1;
-    JProgressBar progressBar = new JProgressBar();
+    private JLabel pressToStartLabel;
+    private JProgressBar progressBar = new JProgressBar();
+    private Timer timer;
+    private Timer parpadeo;
+    private int counter = 1;
 
-    private void animation() {//Animación para cargar el juego
+    private void animation() {
         timer = new Timer(10, (e) -> {
             progressBar.setValue(counter);
             counter++;
@@ -28,7 +30,7 @@ public class Main extends JFrame {
         timer.start();
     }
 
-    private void help() {//Método para abrir la pantalla de ayuda
+    private void help() {
         this.setVisible(false);
         this.dispose();
         helpScreen help = new helpScreen();
@@ -36,7 +38,7 @@ public class Main extends JFrame {
         help.setLocationRelativeTo(null);
     }
 
-    private void customizePlayer() {//Método para abrir la pantalla de personalización
+    private void customizePlayer() {
         this.setVisible(false);
         this.dispose();
         nameScreen customizeScreen = new nameScreen();
@@ -44,16 +46,14 @@ public class Main extends JFrame {
         customizeScreen.setLocationRelativeTo(null);
     }
 
-    public static void main(String[] args) {//Método main para ejecutar el programa
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    Main frame = new Main();
-                    frame.setVisible(true);
-                    frame.setLocationRelativeTo(null);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                Main frame = new Main();
+                frame.setVisible(true);
+                frame.setLocationRelativeTo(null);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
     }
@@ -76,6 +76,20 @@ public class Main extends JFrame {
         logoLabel.setBounds(30, 20, 300, 200);
         panelPrincipal.add(logoLabel);
 
+        //Texto de inicio
+        pressToStartLabel = new JLabel("Press any key to start");
+        pressToStartLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        pressToStartLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
+        pressToStartLabel.setForeground(Color.WHITE);
+        pressToStartLabel.setBounds(35, 200, 278, 30);
+        panelPrincipal.add(pressToStartLabel);
+
+        //Parpadeo del texto
+        parpadeo = new Timer(500, e -> {
+            pressToStartLabel.setVisible(!pressToStartLabel.isVisible());
+        });
+        parpadeo.start();
+
         //Barra de progreso
         progressBar.setBackground(new Color(240, 240, 240));
         progressBar.setForeground(new Color(255, 216, 56));
@@ -86,43 +100,72 @@ public class Main extends JFrame {
         progressBar.setVisible(false);
         panelPrincipal.add(progressBar);
 
-        //Botón de inicio
-        JButton btnStartGame = new JButton("START");
-        btnStartGame.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                animation();
-                progressBar.setVisible(true);
-            }
-        });
-        btnStartGame.setFocusable(false);
-        btnStartGame.setBackground(new Color(255, 216, 56));
-        btnStartGame.setForeground(new Color(240, 240, 240));
-        btnStartGame.setFont(new Font("Tahoma", Font.BOLD, 28));
-        btnStartGame.setBounds(53, 237, 241, 55);
-        panelPrincipal.add(btnStartGame);
-
         //Botón de personalización
         JButton btnCustomize = new JButton("CUSTOMIZE");
         btnCustomize.setFocusable(false);
-        btnCustomize.addActionListener(e -> customizePlayer());
+        btnCustomize.addActionListener(e -> {
+            parpadeo.stop();
+            pressToStartLabel.setVisible(false);
+            customizePlayer();
+        });
         btnCustomize.setForeground(new Color(240, 240, 240));
         btnCustomize.setBackground(new Color(255, 216, 56));
         btnCustomize.setFont(new Font("Tahoma", Font.BOLD, 28));
-        btnCustomize.setBounds(53, 313, 241, 55);
+        btnCustomize.setBounds(53, 250, 241, 55);
         panelPrincipal.add(btnCustomize);
 
         //Botón de ayuda
         JButton btnHelp = new JButton("HELP");
-        btnHelp.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                help();
-            }
+        btnHelp.addActionListener(e -> {
+            parpadeo.stop();
+            pressToStartLabel.setVisible(false);
+            help();
         });
         btnHelp.setFocusable(false);
         btnHelp.setBackground(new Color(255, 216, 56));
         btnHelp.setForeground(new Color(240, 240, 240));
         btnHelp.setFont(new Font("Tahoma", Font.BOLD, 28));
-        btnHelp.setBounds(53, 390, 241, 55);
+        btnHelp.setBounds(53, 320, 241, 55);
         panelPrincipal.add(btnHelp);
+
+        //Boton información
+        JButton btnInfo = new JButton("!");
+        btnInfo.setFocusable(false);
+        btnInfo.addActionListener(e -> {
+            JOptionPane.showMessageDialog(this, "Street Fighter Lite v1.0\nDesarrollado por Hector", "Información",
+                    JOptionPane.INFORMATION_MESSAGE);
+        });
+        btnInfo.setForeground(new Color(240, 240, 240));
+        btnInfo.setBackground(new Color(255, 216, 56)); 
+        btnInfo.setFont(new Font("Tahoma", Font.BOLD, 30));
+        btnInfo.setBounds(140, 390, 60, 40);
+        panelPrincipal.add(btnInfo);
+
+        //Listener de eventos para el panel principal
+        panelPrincipal.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (!progressBar.isVisible()) {
+                    parpadeo.stop();
+                    pressToStartLabel.setVisible(false);
+                    progressBar.setVisible(true);
+                    animation();
+                }
+            }
+        });
+
+        panelPrincipal.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (!progressBar.isVisible()) {
+                    parpadeo.stop();
+                    pressToStartLabel.setVisible(false);
+                    progressBar.setVisible(true);
+                    animation();
+                }
+            }
+        });
+        panelPrincipal.setFocusable(true);
+        panelPrincipal.requestFocusInWindow();
     }
 }
