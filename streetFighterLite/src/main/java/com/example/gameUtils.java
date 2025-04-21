75b845e7b3fbe6540833fafbd86eac7b4baf2af5
hector-rodri <hrodriguez@nigul.cide.es>
2025-04-21 14:14:47 +0200

    interfaz con las constantes necesarias para el juego

---
+++ streetFighterLite/src/main/java/com/example/gameUtils.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public interface gameUtils { //Interfaz para almacenar constantes del juego
+    int FRAME_HEIGHT = 600; 
+    int FRAME_WIDTH = 800; 
+    String TITLE = "Street Fighter"; 
+    String KEN_IMAGE = "SpriteKen.png"; 
+    String RYU_IMAGE = "SpriteRyu.png"; 
+    String BG_IMAGE = "StreetFighterBG.png"; 
+    int FLOOR = FRAME_HEIGHT - 80; 
+    int DELAY = 100; 
+    int TIME_LEFT = 50; 
+}
