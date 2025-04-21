bbfea0828f89dd8af7e7184fa9564dc90af0410f
hector-rodri <hrodriguez@nigul.cide.es>
2025-04-21 19:38:06 +0200

    look and feel streetFighter

--- streetFighterLite/src/main/java/com/example/helpScreen.java
+++ streetFighterLite/src/main/java/com/example/helpScreen.java
@@ -3,123 +3,119 @@ package com.example;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import javax.swing.*;
-import java.awt.event.*;
 
 public class helpScreen extends JFrame {
 
     private JPanel contentPane;
 
     public helpScreen() {
-
+        setTitle("STREET FIGHTER LITE - HELP");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 501, 447);
+        setBounds(100, 100, 600, 500);
         contentPane = new JPanel();
-        contentPane.setBackground(new Color(44, 150, 57));
+        contentPane.setBackground(new Color(255, 56, 56));
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPane);
         contentPane.setLayout(null);
         setResizable(false);
 
         JLabel labelInstructions = new JLabel("INSTRUCTIONS");
-        labelInstructions.setForeground(Color.black);
-        labelInstructions.setFont(new Font("Tahoma", Font.BOLD, 25));
-        labelInstructions.setBounds(157, 20, 200, 46);
+        labelInstructions.setForeground(Color.WHITE);
+        labelInstructions.setFont(new Font("Tahoma", Font.BOLD, 30));
+        labelInstructions.setBounds(180, 20, 300, 40);
         contentPane.add(labelInstructions);
 
-        JLabel jugador1 = new JLabel("RYU PLAYER 1");
-        jugador1.setFont(new Font("Tahoma", Font.BOLD, 19));
-        jugador1.setBounds(20, 75, 304, 29);
-        jugador1.setForeground(new Color(49, 47, 47));
+        JLabel jugador1 = new JLabel("RYU - PLAYER 1");
+        jugador1.setFont(new Font("Tahoma", Font.BOLD, 22));
+        jugador1.setBounds(50, 80, 300, 30);
+        jugador1.setForeground(new Color(240, 240, 240));
         contentPane.add(jugador1);
 
         JLabel labelVkupIncrease = new JLabel("Key (W): Jump");
-        labelVkupIncrease.setFont(new Font("Tahoma", Font.ITALIC, 18)); 
-        labelVkupIncrease.setBounds(20, 100, 304, 29); 
-        labelVkupIncrease.setForeground(new Color(235, 235, 235));
+        labelVkupIncrease.setFont(new Font("Tahoma", Font.ITALIC, 18));
+        labelVkupIncrease.setBounds(50, 120, 300, 30);
+        labelVkupIncrease.setForeground(new Color(240, 240, 240));
         contentPane.add(labelVkupIncrease);
 
-        JLabel labelVkrightIncrease = new JLabel("Key (A): Left");
+        JLabel labelVkrightIncrease = new JLabel("Key (A): Move Left");
         labelVkrightIncrease.setFont(new Font("Tahoma", Font.ITALIC, 18));
-        labelVkrightIncrease.setBounds(20, 135, 304, 29);
-        labelVkrightIncrease.setForeground(new Color(235, 235, 235));
+        labelVkrightIncrease.setBounds(50, 160, 300, 30);
+        labelVkrightIncrease.setForeground(new Color(240, 240, 240));
         contentPane.add(labelVkrightIncrease);
 
-        JLabel labelVkdownDecrease = new JLabel("Key (D): Right");
+        JLabel labelVkdownDecrease = new JLabel("Key (D): Move Right");
         labelVkdownDecrease.setFont(new Font("Tahoma", Font.ITALIC, 18));
-        labelVkdownDecrease.setBounds(20, 170, 304, 29);
-        labelVkdownDecrease.setForeground(new Color(235, 235, 235));
+        labelVkdownDecrease.setBounds(50, 200, 300, 30);
+        labelVkdownDecrease.setForeground(new Color(240, 240, 240));
         contentPane.add(labelVkdownDecrease);
 
         JLabel labelVkleftDecrease = new JLabel("Key (G): Punch");
         labelVkleftDecrease.setFont(new Font("Tahoma", Font.ITALIC, 18));
-        labelVkleftDecrease.setBounds(20, 205, 304, 29);
-        labelVkleftDecrease.setForeground(new Color(235, 235, 235));
+        labelVkleftDecrease.setBounds(50, 240, 300, 30);
+        labelVkleftDecrease.setForeground(new Color(240, 240, 240));
         contentPane.add(labelVkleftDecrease);
 
         JLabel labelVkrIncrease = new JLabel("Key (H): Kick");
         labelVkrIncrease.setFont(new Font("Tahoma", Font.ITALIC, 18));
-        labelVkrIncrease.setBounds(20, 240, 304, 29);
-        labelVkrIncrease.setForeground(new Color(235, 235, 235));
+        labelVkrIncrease.setBounds(50, 280, 300, 30);
+        labelVkrIncrease.setForeground(new Color(240, 240, 240));
         contentPane.add(labelVkrIncrease);
 
         JLabel labelVklDecrease = new JLabel("Key (E): Defend");
         labelVklDecrease.setFont(new Font("Tahoma", Font.ITALIC, 18));
-        labelVklDecrease.setBounds(20, 275, 304, 29);
-        labelVklDecrease.setForeground(new Color(235, 235, 235));
+        labelVklDecrease.setBounds(50, 320, 300, 30);
+        labelVklDecrease.setForeground(new Color(240, 240, 240));
         contentPane.add(labelVklDecrease);
 
-        JLabel jugador2 = new JLabel("KEN PLAYER 2");
-        jugador2.setFont(new Font("Tahoma", Font.BOLD, 19));
-        jugador2.setBounds(280, 75, 304, 29);
-        jugador2.setForeground(new Color(49, 47, 47));
+        JLabel jugador2 = new JLabel("KEN - PLAYER 2");
+        jugador2.setFont(new Font("Tahoma", Font.BOLD, 22));
+        jugador2.setBounds(350, 80, 300, 30);
+        jugador2.setForeground(new Color(240, 240, 240));
         contentPane.add(jugador2);
 
         JLabel labelVkupIncrease2 = new JLabel("Arrow Key: Jump");
         labelVkupIncrease2.setFont(new Font("Tahoma", Font.ITALIC, 18));
-        labelVkupIncrease2.setBounds(280, 100, 304, 29);
-        labelVkupIncrease2.setForeground(new Color(235, 235, 235));
+        labelVkupIncrease2.setBounds(350, 120, 300, 30);
+        labelVkupIncrease2.setForeground(new Color(240, 240, 240));
         contentPane.add(labelVkupIncrease2);
 
-        JLabel labelVkrightIncrease2 = new JLabel("Arrow Key: Left");
+        JLabel labelVkrightIncrease2 = new JLabel("Arrow Key: Move Left");
         labelVkrightIncrease2.setFont(new Font("Tahoma", Font.ITALIC, 18));
-        labelVkrightIncrease2.setBounds(280, 135, 304, 29);
-        labelVkrightIncrease2.setForeground(new Color(235, 235, 235));
+        labelVkrightIncrease2.setBounds(350, 160, 300, 30);
+        labelVkrightIncrease2.setForeground(new Color(240, 240, 240));
         contentPane.add(labelVkrightIncrease2);
-        
-        JLabel labelVkleftDecrease2 = new JLabel("Arrow Key: Right");
+
+        JLabel labelVkleftDecrease2 = new JLabel("Arrow Key: Move Right");
         labelVkleftDecrease2.setFont(new Font("Tahoma", Font.ITALIC, 18));
-        labelVkleftDecrease2.setBounds(280, 170, 304, 29);
-        labelVkleftDecrease2.setForeground(new Color(235, 235, 235));
+        labelVkleftDecrease2.setBounds(350, 200, 300, 30);
+        labelVkleftDecrease2.setForeground(new Color(240, 240, 240));
         contentPane.add(labelVkleftDecrease2);
-        
+
         JLabel labelVkrIncrease2 = new JLabel("NumPad (1): Punch");
         labelVkrIncrease2.setFont(new Font("Tahoma", Font.ITALIC, 18));
-        labelVkrIncrease2.setBounds(280, 205, 304, 29);
-        labelVkrIncrease2.setForeground(new Color(235, 235, 235));
+        labelVkrIncrease2.setBounds(350, 240, 300, 30);
+        labelVkrIncrease2.setForeground(new Color(240, 240, 240));
         contentPane.add(labelVkrIncrease2);
-        
+
         JLabel labelVklDecrease2 = new JLabel("NumPad (2): Kick");
         labelVklDecrease2.setFont(new Font("Tahoma", Font.ITALIC, 18));
-        labelVklDecrease2.setBounds(280, 240, 304, 29);
-        labelVklDecrease2.setForeground(new Color(235, 235, 235));
+        labelVklDecrease2.setBounds(350, 280, 300, 30);
+        labelVklDecrease2.setForeground(new Color(240, 240, 240));
         contentPane.add(labelVklDecrease2);
-        
+
         JLabel labelVklPoderespecial2 = new JLabel("NumPad (3): Defend");
         labelVklPoderespecial2.setFont(new Font("Tahoma", Font.ITALIC, 18));
-        labelVklPoderespecial2.setBounds(280, 275, 304, 29);
-        labelVklPoderespecial2.setForeground(new Color(235, 235, 235));
+        labelVklPoderespecial2.setBounds(350, 320, 300, 30);
+        labelVklPoderespecial2.setForeground(new Color(240, 240, 240));
         contentPane.add(labelVklPoderespecial2);
-        
-        JButton buttonExit = new JButton("EXIT");
-        buttonExit.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                exit();
-            }
-        });
-        buttonExit.setFont(new Font("Tahoma", Font.BOLD, 28));
-        buttonExit.setBackground(new Color(235, 235, 235));
-        buttonExit.setForeground(new Color(44, 150, 57));
-        buttonExit.setBounds(177, 365, 150, 40);
+
+        JButton buttonExit = new JButton("BACK");
+        buttonExit.addActionListener(e -> exit());
+        buttonExit.setFocusable(false);
+        buttonExit.setFont(new Font("Tahoma", Font.BOLD, 20));
+        buttonExit.setBackground(new Color(255, 216, 56));
+        buttonExit.setForeground(new Color(240, 240, 240));
+        buttonExit.setBounds(230, 400, 120, 40);
         contentPane.add(buttonExit);
     }
 
--- streetFighterLite/src/main/java/com/example/nameScreen.java
+++ streetFighterLite/src/main/java/com/example/nameScreen.java
@@ -1,95 +1,83 @@
 package com.example;
 
-import javax.swing.*;//Imports necesarios para la interfaz gráfica y el diseño
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.*;
 
-public class nameScreen extends JFrame {//Define la clase que hereda de JFrame
-    public JTextField nombreJugador1;//Campos de texto donde los jugadores escribirán sus nombres
-    public JTextField nombreJugador2;
+public class nameScreen extends JFrame {
+
+    private JTextField nombreJugador1;
+    private JTextField nombreJugador2;
 
     public nameScreen() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Cierra la aplicación al cerrar la ventana
-        setBounds(100, 100, 501, 447);//Establece el tamaño y posición 
-        setLocationRelativeTo(null);//Centra la ventana 
-        setResizable(false); // Evita que la ventana sea redimensionable
+        setTitle("STREET FIGHTER LITE - CUSTOMIZE");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 600, 500);
+        setLocationRelativeTo(null);
+        setResizable(false);
 
-        //PANEL PRINCIPAL
-        JPanel panelPrincipal = new JPanel();//Crea el panel principal de la ventana
-        panelPrincipal.setBackground(new Color(232, 232, 232));
-        setContentPane(panelPrincipal);//El panel es el contenido principal de la ventana
-        panelPrincipal.setLayout(null);//Usa un diseño absoluto para posicionar los componentes manualmente
+        JPanel panelPrincipal = new JPanel();
+        panelPrincipal.setBackground(new Color(255, 56, 56));
+        panelPrincipal.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(panelPrincipal);
+        panelPrincipal.setLayout(null);
 
-        //TITULO
-        JLabel titulo = new JLabel("");//Crea un título
-        titulo.setFont(new Font("Tahoma", Font.BOLD, 30));//Fuente y tamaño
-        titulo.setBounds(80, 40, 400, 46);//Posición y tamaño
+        JLabel titulo = new JLabel("CUSTOMIZE PLAYERS");
+        titulo.setFont(new Font("Tahoma", Font.BOLD, 30));
+        titulo.setForeground(Color.WHITE);
+        titulo.setBounds(130, 30, 400, 40);
         panelPrincipal.add(titulo);
 
-        //JUGADOR 1
-        JLabel jugador1 = new JLabel("PLAYER 1:");//Etiqueta para el primer jugador
-        jugador1.setFont(new Font("Tahoma", Font.BOLD, 19));//Fuente y tamaño
-        jugador1.setBounds(80, 120, 150, 30);//Posición y tamaño
+        JLabel jugador1 = new JLabel("PLAYER 1:");
+        jugador1.setFont(new Font("Tahoma", Font.BOLD, 22));
+        jugador1.setForeground(Color.WHITE);
+        jugador1.setBounds(100, 120, 150, 30);
         panelPrincipal.add(jugador1);
 
-        nombreJugador1 = new JTextField();//Campo de texto para el nombre del jugador
-        nombreJugador1.setFont(new Font("Tahoma", Font.PLAIN, 16));//Fuente y tamaño
-        nombreJugador1.setBounds(200, 120, 210, 30);//Posición y tamaño
+        nombreJugador1 = new JTextField();
+        nombreJugador1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        nombreJugador1.setBounds(220, 120, 250, 30);
         panelPrincipal.add(nombreJugador1);
 
-        //JUGADOR 2
-        JLabel jugador2 = new JLabel("PLAYER 2:");//Etiqueta para el segundo jugador
-        jugador2.setFont(new Font("Tahoma", Font.BOLD, 19));//Fuente y tamaño
-        jugador2.setBounds(80, 170, 150, 30);//Posición y tamaño
+        JLabel jugador2 = new JLabel("PLAYER 2:");
+        jugador2.setFont(new Font("Tahoma", Font.BOLD, 22));
+        jugador2.setForeground(Color.WHITE);
+        jugador2.setBounds(100, 180, 150, 30);
         panelPrincipal.add(jugador2);
 
-        nombreJugador2 = new JTextField();//Campo de texto para el nombre del jugador
-        nombreJugador2.setFont(new Font("Tahoma", Font.PLAIN, 16));//Fuente y tamaño
-        nombreJugador2.setBounds(200, 170, 210, 30);//Posición y tamaño
+        nombreJugador2 = new JTextField();
+        nombreJugador2.setFont(new Font("Tahoma", Font.PLAIN, 18));
+        nombreJugador2.setBounds(220, 180, 250, 30);
         panelPrincipal.add(nombreJugador2);
 
-        //BOTÓN SALIR
-        JButton botonSalir = new JButton("EXIT");//Botón para botonSalir
-        botonSalir.setFont(new Font("Tahoma", Font.BOLD, 26));//Fuente y tamaño
-        botonSalir.setBackground(new Color(44, 150, 57));//Color de fondo
-        botonSalir.setForeground(new Color(235, 235, 235));//Color del texto
-        botonSalir.setBounds(80, 240, 150, 50);//Posición y tamaño
-        botonSalir.addActionListener(new ActionListener() {//Cuando se hace clic en el botón, se ejecuta el método salir
-            @Override
-            public void actionPerformed(ActionEvent event) {
-                salir();
-            }
-        });
-        panelPrincipal.add(botonSalir);
-
-        //BOTÓN GUARDAR
-        JButton botonGuardar = new JButton("SAVE");//Botón para botonGuardar los nombres
-        botonGuardar.setFont(new Font("Tahoma", Font.BOLD, 26));//Fuente y tamaño
-        botonGuardar.setForeground(new Color(235, 235, 235));//Color del texto
-        botonGuardar.setBackground(new Color(44, 150, 57));//Color de fondo
-        botonGuardar.setFocusPainted(false);//Quita el contorno al hacer clic
-        botonGuardar.setBorderPainted(false);//Quita el borde del botón
-        botonGuardar.setBounds(260, 240, 150, 50);//Posición y tamaño
-        botonGuardar.addActionListener(new ActionListener() {//Cuando se hace clic en el botón, se ejecuta el método guardar
-            @Override
-            public void actionPerformed(ActionEvent event) {
-                guardar();
-            }
-        });
+        JButton botonGuardar = new JButton("SAVE");
+        botonGuardar.setFont(new Font("Tahoma", Font.BOLD, 22));
+        botonGuardar.setForeground(new Color(255, 56, 56));
+        botonGuardar.setBackground(new Color(255, 216, 56));
+        botonGuardar.setBounds(80, 280, 200, 50);
+        botonGuardar.addActionListener(e -> guardar());
         panelPrincipal.add(botonGuardar);
+
+        JButton botonSalir = new JButton("BACK");
+        botonSalir.setFont(new Font("Tahoma", Font.BOLD, 22));
+        botonSalir.setForeground(new Color(255, 56, 56));
+        botonSalir.setBackground(new Color(255, 216, 56));
+        botonSalir.setBounds(300, 280, 200, 50);
+        botonSalir.addActionListener(e -> salir());
+        panelPrincipal.add(botonSalir);
     }
 
-    public void guardar() {//Método para guardar los nombres de los jugadores.
-        String jugadorNombre1 = nombreJugador1.getText().trim();//Obtiene el nombre de los jugadores
+    public void guardar() {
+        String jugadorNombre1 = nombreJugador1.getText().trim();
         String jugadorNombre2 = nombreJugador2.getText().trim();
 
-        if (jugadorNombre1.isEmpty() || jugadorNombre2.isEmpty()) {//Comprueba si ningún campo de texto está vacío
+        if (jugadorNombre1.isEmpty() || jugadorNombre2.isEmpty()) {
             JOptionPane.showMessageDialog(this, "Please enter names to continue", "Error", JOptionPane.ERROR_MESSAGE);
-            return;//Muestra un mensaje de error
+            return;
         }
 
-        juego juego = new juego(this);//Crea una ventana de juego
-        juego.setPlayerNames(jugadorNombre1.toUpperCase(), jugadorNombre2.toUpperCase());// Establece los nombres introducidos
+        juego juego = new juego(this);
+        juego.setPlayerNames(jugadorNombre1.toUpperCase(), jugadorNombre2.toUpperCase());
         JFrame game = new JFrame();
         game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         game.setSize(800, 600);
@@ -97,17 +85,15 @@ public class nameScreen extends JFrame {//Define la clase que hereda de JFrame
         game.add(juego);
         game.setVisible(true);
 
-        //Asegurarse de que se cierre la ventana actual
-        this.setVisible(false);//Oculta la ventana actual
-        this.dispose();//Libera los recursos de la ventana actual
-
+        this.setVisible(false);
+        this.dispose();
     }
 
-    public void salir() {//Método para salir de la pantalla
-        this.setVisible(false);//Oculta la ventana actual
+    public void salir() {
+        this.setVisible(false);
         this.dispose();
-        Main frame = new Main();//Crea una nueva ventana del menu inicial
-        frame.setVisible(true);//Muestra la nueva pantalla.
+        Main frame = new Main();
+        frame.setVisible(true);
         frame.setLocationRelativeTo(null);
     }
 }
