fa1bb4ae8aa6d38c2abb3fbcf5747433472829b3
hector-rodri <hrodriguez@nigul.cide.es>
2025-05-05 18:43:42 +0200

    comienzo del juego y mensaje de bienvenida

--- piedra_papel_tijera/src/main/java/com/example/Main.java
+++ piedra_papel_tijera/src/main/java/com/example/Main.java
@@ -1,7 +1,20 @@
 package com.example;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] opciones = { "piedra", "papel", "tijera" };
+
+        System.out.println("BIENVENIDO A PIEDRA, PAPEL O TIJERA!");
+        System.out.println("Dime tu nombre:");
+        String nombre = sc.nextLine();
+        System.out.println("Hola " + nombre);
+
+        sc.close();
     }
 }
