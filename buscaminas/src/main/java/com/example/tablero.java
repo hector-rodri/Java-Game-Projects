91ced2a7e18d3930a54ee073eaa1a0bfca38ea8d
hector-rodri <hrodriguez@nigul.cide.es>
2025-06-03 21:17:16 +0200

    parametro numero de minas

--- buscaminas/src/main/java/com/example/tablero.java
+++ buscaminas/src/main/java/com/example/tablero.java
@@ -5,10 +5,12 @@ public class tablero {
     private casilla[][] casillas;
     private int numeroFilas;
     private int numeroColumnas;
+    private int numeroMinas;
     
-    public tablero(int numeroFilas, int numeroColumnas) {
+    public tablero(int numeroFilas, int numeroColumnas, int numeroMinas) {
         this.numeroFilas = numeroFilas;
         this.numeroColumnas = numeroColumnas;
+        this.numeroMinas = numeroMinas;
     }
 
     public void inicializarCasillas() {
@@ -18,6 +20,10 @@ public class tablero {
                 casillas[i][j] = new casilla(i, j);
             }
         }
+    }
+
+    public void generarMinas(){
+        
     }
 
 }
